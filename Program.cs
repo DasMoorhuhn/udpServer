b68b4c73e96d208b1134a686ef4feb40c82e05c4
Hendrik Heine <hendrik@ruckelshaeuser.de>
2021-07-08 19:22:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using udpServer.Server;
+using System.Net.Sockets;
+
+namespace udpServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Server.Server server = new Server.Server(12000);
+			Socket sock = server.getSock();
+			Console.Title = "UDP Server";
+
+			while (true)
+			{
+				string recv = server.startListen();
+				Console.WriteLine(recv);
+			}
+		}
+	}
+}
---
+++ Server/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace udpServer.Server
+{
+	class Server
+	{
+		private int serverPort;
+		private Socket sock;
+		private UdpClient listen;
+		private IPEndPoint ep;
+		public Server(int _serverPort)
+		{
+			this.serverPort = _serverPort;
+			this.sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			this.listen = new UdpClient(this.serverPort);
+			this.ep = new IPEndPoint(IPAddress.Any, this.serverPort);
+		}
+
+		public Socket getSock()
+		{
+			return this.sock;
+		}
+
+		public string startListen()
+		{
+			byte[] recv = this.listen.Receive(ref this.ep);
+			return Encoding.ASCII.GetString(recv, 0, recv.Length);
+		}
+	}
+}

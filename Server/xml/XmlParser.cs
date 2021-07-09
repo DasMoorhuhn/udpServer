62454cca3155066c271af2c80bd9278ee7d962b6
Hendrik Heine <hendrik@ruckelshaeuser.de>
2021-07-09 20:49:15 +0200

    basic

--- Server/xml/XmlParser.cs
+++ Server/xml/XmlParser.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace udpServer.Server.xml
 {
 	class XmlParser
 	{
+		XmlDocument doc;
+		public XmlParser()
+		{
+			this.doc = new XmlDocument();
+			doc.PreserveWhitespace = true;
+		}
+
+		public void createXmlDoc(XmlDocument _doc)
+		{
+		}
 	}
 }
